654d8b239f666d470bf5b6ad9803cf85e93526ec
Carlo Contardi <c.contardi@spacecoding.it>
2024-12-30 17:49:19 +0100

    [Feature] Add ui and count button

--- app/src/main/java/it/spacecoding/sharedpreferences/MainActivity.java
+++ app/src/main/java/it/spacecoding/sharedpreferences/MainActivity.java
@@ -1,6 +1,10 @@
 package it.spacecoding.sharedpreferences;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +13,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText userName;
+    EditText userMessage;
+    Button counter;
+    CheckBox remember;
+    int count = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +28,18 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        userName = findViewById(R.id.editTextName);
+        userMessage = findViewById(R.id.editTextMessage);
+        counter = findViewById(R.id.button);
+        remember = findViewById(R.id.checkBox);
+
+        counter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                count++;
+                counter.setText(String.valueOf(count));
+            }
+        });
+
     }
 }
